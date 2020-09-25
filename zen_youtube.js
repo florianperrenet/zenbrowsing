821d8982a7ec10d3be53ddd6a6192ce1ce7fffe8
Florian Perrenet <florian.perrenet@gmail.com>
2020-09-25 12:24:02 +0200

    added youtube, zens out recommendations and related videos

---
+++ zen_youtube.js
@@ -0,0 +1,27 @@
+const hidepaths = ['/', '/feed/trending'];
+
+function el(id) {
+	return document.getElementById(id);
+}
+
+function update_style(el, css) {
+	if (el) el.style.cssText = css;
+}
+
+function hide() {
+	update_style(el('contents'), 'visibility: hidden;');
+	update_style(el('page-manager'), 'visibility: hidden;');
+}
+
+function show() {
+	update_style(el('contents'), 'visibility: visible;');
+	update_style(el('page-manager'), 'visibility: visible;');
+}
+
+document.addEventListener('yt-page-data-updated', e => {
+	// hide related videos
+	update_style(el('related'), 'display: none;');
+
+	// hide or show recommendations
+	hidepaths.includes(location.pathname) ? hide() : show();
+});
